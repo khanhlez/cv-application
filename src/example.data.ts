21757d627da3636a79d7dfab1258e38a2dbffaf0
KhanhLe <kl@cct-technology.com>
2024-01-28 08:20:24 +0700

    Update README and sample data

--- src/example.data.ts
+++ src/example.data.ts
@@ -2,10 +2,10 @@ import uniqid from "uniqid";
 
 const exampleData = {
 	personalInfo: {
-		fullName: "Josephine Meyers",
-		email: "josephine.meyers@mail.co.uk",
-		phoneNumber: "+44 3245 5521 5521",
-		address: "London, UK",
+		fullName: "Khanh Le",
+		email: "khanhle.developer@gmail.com",
+		phoneNumber: "+84 979 123 4567",
+		address: "Vietnam, VN",
 	},
 
 	sections: {
